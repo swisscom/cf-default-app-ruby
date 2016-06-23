e12948fb4301c07e448ead94fecf740256299a43
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2016-06-23 15:04:16 +0200

    use ruby style variables

--- app.rb
+++ app.rb
@@ -7,9 +7,9 @@ set :public_folder, File.dirname(__FILE__) + '/public'
 
 # our basic route which renders the index view
 get '/' do
-  @envVariables = ENV.sort
-  @headers = env.inject({}){|acc, (k,v)| acc[$1.downcase] = v if k =~ /^http_(.*)/i; acc}.sort
-  @params = params.sort
+  @env_variables = ENV.sort
+  @req_headers = env.inject({}){|acc, (k,v)| acc[$1.downcase] = v if k =~ /^http_(.*)/i; acc}.sort
+  @req_params = params.sort
   haml :index
 end
 
